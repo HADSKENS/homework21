1322e6879d98cf1327bb0a1ca032e56fe7b12d52
Handsken <nan.222@yandex.ru>
2023-02-21 00:24:50 +0500

    complited 1 exercise

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,29 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
         in(list);
         list.stream()
-                .filter(x -> x % 2 == 0)
+                .filter(x-> x%2==0)
                 .forEach(System.out::println);
+        long sorted = list.stream()
+                .filter(x->x%2==0)
+                .count();
+        System.out.println("Кол-во четных чисел "+sorted);
     }
-
-    public static void in(List<Integer> list) {
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-        list.add(6);
-        list.add(7);
-        list.add(8);
-        list.add(9);
-        list.add(10);
+        public static void in (List < Integer > list) {
+            list.add(1);
+            list.add(2);
+            list.add(3);
+            list.add(4);
+            list.add(5);
+            list.add(6);
+            list.add(7);
+            list.add(8);
+            list.add(9);
+            list.add(10);
     }
 }
