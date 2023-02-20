e040f7ee72568de25604f4ebc234fbf80853fb9b
Handsken <nan.222@yandex.ru>
2023-02-20 15:04:02 +0500

    complited 1 exercise

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,25 @@
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();in(list);
+        List<Integer> list = new ArrayList<>();
+        in(list);
         list.stream()
-                .filter(x -> x%2==0)
+                .filter(x -> x % 2 == 0)
                 .forEach(System.out::println);
     }
-        public static void in (List<Integer> list){
-            list.add(1);
-            list.add(2);
-            list.add(3);
-            list.add(4);
-            list.add(5);
-            list.add(6);
-            list.add(7);
-            list.add(8);
-            list.add(9);
-            list.add(10);
+
+    public static void in(List<Integer> list) {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        list.add(10);
     }
 }
