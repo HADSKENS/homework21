aaa65d4f298c4705ef7424f367d62e040530c3a6
Handsken <nan.222@yandex.ru>
2023-02-23 20:09:08 +0500

    complited

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,52 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
+
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<Integer>();
         in(list);
         list.stream()
+                .filter(x -> x % 2 == 0)
                 .filter(x-> x%2==0)
                 .forEach(System.out::println);
         long sorted = list.stream()
                 .filter(x->x%2==0)
                 .count();
         System.out.println("Кол-во четных чисел "+sorted);
+
+        findMinMax(
+                list.stream(),
+                Comparator.naturalOrder(),
+                (x, y) -> System.out.printf("min %s, max %s %n%n", x, y)
+        );
     }
-        public static void in (List < Integer > list) {
+
+    public static <T>  void findMinMax(
+            Stream<? extends T> stream,
+            Comparator <? super T> order,
+            BiConsumer<? super T, ? super T>minMaxConsumer) {
+        List<T> abc = new ArrayList<>();
+        T min =null;
+        T max =null;
+        if (stream != null) {
+            abc=stream
+                    .sorted(order)
+                    .collect(Collectors.toList());
+            if (abc.size() !=0){
+                min = abc.get(0);
+                max = abc.get(abc.size()-1);
+                minMaxConsumer.accept(min,max);
+            }
+            else {
+                minMaxConsumer.accept(null,null);
+            }
+        }
+    }
+
+    public static void in (List < Integer > list) {
             list.add(1);
             list.add(2);
             list.add(3);
